f30e34c573c18968e2a5a066c115ed023a796605
aravindhravindran <aravindh.ravindran@redbus.com>
2018-12-03 10:23:09 +0530

    Validating words by comparing with objectionable words initialized.

--- src/main/java/com/aravindh/commentFeedback/service/FeedBackValidationService.java
+++ src/main/java/com/aravindh/commentFeedback/service/FeedBackValidationService.java
@@ -1,10 +1,40 @@
 package com.aravindh.commentFeedback.service;
 
+import spark.utils.StringUtils;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FeedBackValidationService {
+    private static List<String> objectionableWords = Arrays.asList("worst", "bad");
+    private static String SPACE = " ";
+
     public static List<String> validate(String body) {
-        return Collections.EMPTY_LIST;
+        if(StringUtils.isEmpty(body)){
+            return Collections.EMPTY_LIST;
+        }
+        List<String> feedback = new LinkedList<>();
+        validateWords(body, feedback);
+        return feedback;
     }
+
+    private static void validateWords(String body, List<String> feedback) {
+        String[] words = body.split(SPACE);
+        for(String word : words){
+            if(containsCaseInsensitive(objectionableWords, word)){
+                feedback.add(word+": objectionable content");
+            }
+        }
+    }
+
+    private static boolean containsCaseInsensitive(List<String> objectionableWords, String word) {
+        for(String objectionableWord:objectionableWords){
+            if(word.equalsIgnoreCase(objectionableWord)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
